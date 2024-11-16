8a6f39c7f02862084b4c0a9de0f6f77ae4caa3d3
mohammadali koutahi <mmliinjas@gmail.com>
2024-11-16 23:20:55 +0330

    when user password != password2 the username and email still in box for better experience for user

--- myapp/views.py
+++ myapp/views.py
@@ -13,20 +13,19 @@ def register(request):
         email=request.POST['email']
         password=request.POST['password']
         password2=request.POST['password2']
-        if password == password2:
-            if User.objects.filter(email=email).exists():
-                messages.info(request , 'EMAIL IS ALREADY USED')
-                return redirect('register')
-            elif User.objects.filter(username=username).exists():
-                messages.info(request , 'USERNAME IS TAKEN')
-                return redirect(register)
-            else:
-                user=User.objects.create_user(username=username , email=email , password=password)
-                user.save();
-                return redirect('login')
-        else:
-            messages.info(request , 'password not the same')
-            return redirect('register')
+        
+        if password != password2:
+            messages.info(request, 'Passwords do not match')
+            return render(request, 'register.html', {'username': username, 'email': email})
+        
+        if User.objects.filter(email=email).exists():
+            messages.info(request, 'EMAIL IS ALREADY USED')
+            return render(request, 'register.html', {'username': username, 'email': email})
+        
+        if User.objects.filter(username=username).exists():
+            messages.info(request, 'USERNAME IS TAKEN')
+            return render(request, 'register.html', {'username': username, 'email': email})
+
             
     else:    
          return render(request , 'register.html')
