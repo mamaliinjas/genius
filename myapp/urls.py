76758995cad74763cc7babfd3112a1d7c78a34fa
mohammadali koutahi <mmliinjas@gmail.com>
2024-11-20 21:47:19 +0330

    add path for artist page

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,5 +6,10 @@ from . import views
 urlpatterns=[
     path('' , views.home , name='home'),
     path('register' , views.register , name='register'),
-    path('login' , views.login , name='login')
+    path('login' , views.login , name='login'),
+    path('artist/<int:artist_id>/' , views.artist_profile , name='artist_profile'),
+    path('album/<int:album_id>/' , views.album_details , name='album_details'),
+    path('song/<int:song_id>/' , views.song_details , name='song_details'),
+    path('track/<int:track_id>/' ,views.track_details , name='track_details'),
+    
 ]
