23227c4af73eb9747b8befd2a9b035591a0a2de2
mohammadali koutahi <mmliinjas@gmail.com>
2024-11-20 22:23:00 +0330

    add minor change for view picture better

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path 
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 
@@ -13,3 +15,5 @@ urlpatterns=[
     path('track/<int:track_id>/' ,views.track_details , name='track_details'),
     
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mygenius/settings.py
+++ mygenius/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=(os.path.join(BASE_DIR , 'static'),)
